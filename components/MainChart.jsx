eaa4574e884225b704c522fde6326ecaab8d6ff0
whynotkimhari <htlulem185@gmail.com>
2023-12-28 13:12:57 +0100

    Fix bug
    + Fix bug when reload the homepage, money in chart will increase

--- components/MainChart.jsx
+++ components/MainChart.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
-  MONTHS,
-  LABELS,
+  getDataForChart,
+  getOptions,
+  getMonths,
   checkMONTHS,
-  getFakeData,
 } from "@utils/mainChartTools";
+
 import {
   Chart as ChartJS,
   BarElement,
@@ -34,78 +35,23 @@ ChartJS.register(
 
 const MainChart = ({ id }) => {
   const year = new Date().getFullYear();
+  const [months, setMonths] = useState([]);
 
-  id &&
-    useEffect(() => {
-      const fetchBilling = async () => {
-        const data = await fetch(`/api/billing/user/${id}/year/${year}`).then(
-          (response) => response.json()
-        );
+  useEffect(() => {
+    const fetchBilling = async () => {
+      const data = await fetch(`/api/billing/user/${id}/year/${year}`).then(
+        (response) => response.json()
+      );
 
-        data.forEach((item) => {
-          MONTHS[item.month - 1].shopping_date.indexOf(item.day) === -1 &&
-            MONTHS[item.month - 1].shopping_date.push(item.day);
-          MONTHS[item.month - 1].total += item.itemPrice * item.itemQuantity;
-        });
-      };
+      setMonths(getMonths(data));
+    };
 
-      fetchBilling();
-    });
+    fetchBilling();
+  }, [id]);
 
-  const checkMonth = checkMONTHS(MONTHS);
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: id ? `Your activity in ${year}` : "THIS IS A FAKE DATA!",
-      },
-    },
-    scales: {
-      "bar-y-axis": {
-        type: "linear",
-        position: "left",
-      },
-      "line-y-axis": {
-        type: "linear",
-        position: "right",
-        ticks: {
-          stepSize: 1,
-        },
-      },
-    },
-  };
-
-  const dataset1 = MONTHS.map((m) => m.shopping_date.length);
-  const dataset2 = MONTHS.map((m) => m.total);
-
-  const [fakeDataset1, fakeDataset2] = getFakeData();
-
-  const data = {
-    labels: LABELS,
-    datasets: [
-      {
-        label: "Shopping times",
-        data: checkMonth ? dataset1 : !id ? fakeDataset1 : [],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-        yAxisID: "line-y-axis",
-        type: "line",
-      },
-      {
-        label: "Total spending (HUF)",
-        data: checkMonth ? dataset2 : !id ? fakeDataset2 : [],
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-        yAxisID: "bar-y-axis",
-        type: "bar",
-      },
-    ],
-  };
+  const checkMonth = checkMONTHS(months);
+  const options = getOptions(year, id);
+  const data = getDataForChart(months, checkMonth, id);
 
   return (
     <section>
--- utils/mainChartTools.js
+++ utils/mainChartTools.js
@@ -1,70 +1,25 @@
+const labels = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
 
-
-export const LABELS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-export const MONTHS = [
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-    {
-        shopping_date: [],
-        total: 0,
-    },
-]
-
-export const checkMONTHS = (months) => {
-    const condA = months.filter(m => m.total !== 0).length !== 0
-    const condB = months.filter(m => m.shopping_date.length !== 0).length !== 0
-    return condA && condB
-}
-
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export const getFakeData = () => {
+const getFakeData = (MONTHS) => {
     return [
         MONTHS.map(() =>
             getRandomInt(0, 20)
@@ -73,4 +28,127 @@ export const getFakeData = () => {
             getRandomInt(0, 50000)
         )
     ]
+}
+
+const getInitMonths = () => {
+    return [
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+        {
+            shopping_date: [],
+            total: 0,
+        },
+    ]
+}
+
+export const checkMONTHS = (months) =>
+    months.filter(m => m.total !== 0).length && months.filter(m => m.shopping_date.length !== 0).length
+
+export const getOptions = (year, id) => {
+    return {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: id ? `Your activity in ${year}` : "THIS IS A FAKE DATA!",
+            },
+        },
+        scales: {
+            "bar-y-axis": {
+                type: "linear",
+                position: "left",
+            },
+            "line-y-axis": {
+                type: "linear",
+                position: "right",
+                ticks: {
+                    stepSize: 1,
+                },
+            },
+        },
+    }
+}
+
+export const getDataForChart = (months, checkMonth, id) => {
+    const dataset1 = months.map((m) => m.shopping_date.length);
+    const dataset2 = months.map((m) => m.total);
+    const [fakeDataset1, fakeDataset2] = getFakeData(months);
+    return {
+        labels,
+        datasets: [
+            {
+                label: "Shopping times",
+                data: checkMonth ? dataset1 : !id ? fakeDataset1 : [],
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+                yAxisID: "line-y-axis",
+                type: "line",
+            },
+            {
+                label: "Total spending (HUF)",
+                data: checkMonth ? dataset2 : !id ? fakeDataset2 : [],
+                borderColor: "rgb(53, 162, 235)",
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+                yAxisID: "bar-y-axis",
+                type: "bar",
+            },
+        ],
+    }
+}
+
+export const getMonths = (data) => {
+    const MONTHS = getInitMonths();
+
+    data.forEach((item) => {
+        MONTHS[item.month - 1].shopping_date.indexOf(item.day) === -1 &&
+            MONTHS[item.month - 1].shopping_date.push(item.day);
+        MONTHS[item.month - 1].total += item.itemPrice * item.itemQuantity;
+    });
+
+    return MONTHS
 }
